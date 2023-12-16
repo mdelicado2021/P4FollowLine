6da9273908b93924f1d2466c7f097472b590dee2
mdelicado2021 <113594726+mdelicado2021@users.noreply.github.com>
2023-12-16 11:48:42 +0100

    Update siguelineas.ino

--- siguelineas/siguelineas.ino
+++ siguelineas/siguelineas.ino
@@ -3,13 +3,9 @@
   De momento código para siguelíneas.
 */
 
-
+#include <Arduino_FreeRTOS.h>
 #include "FastLED.h"
 #include <Servo.h>
-#include <Arduino_FreeRTOS.h>
-#include <FreeRTOSConfig.h>
-#include <task.h>
-#include <semphr.h>
 
 
 //------------------- Definición de variables -------------------
@@ -41,11 +37,22 @@
 // PIN_Motor_PWMB: Analog output [0-255]. It provides speed.
 #define PIN_Motor_PWMB 6
 
-#define VEL 100
 
 #define PIN_RBGLED 4
 #define NUM_LEDS 1
 CRGB leds[NUM_LEDS];
+
+#define VEL 100
+
+// Variables del PD
+float Kp = 1.0; // Constante proporcional
+float Kd = 0.75; // Constante derivativa
+
+float error = 0; // Error actual
+float error_anterior = 0; // Error anterior
+float delta_error = 0; // Cambio en el error
+
+float salidaPD = 0; // Salida del control PD
 
 
 const int SPEED = 9600;
@@ -53,19 +60,150 @@ const int SPEED = 9600;
 int distance;
 int pulse_time;
 
-const int umbral = 650;
+//const int umbral = 650;
+const int umbral = 50;
+
 int leftSensorValue;
 int middleSensorValue;
 int rightSensorValue;
 
-// Declaración de semáforos para sincronización
-SemaphoreHandle_t semaforoInfrarrojo;
-SemaphoreHandle_t semaforoUltrasonido;
 
-// Prototipos de las funciones de las tareas
-void tareaLED(void *parameter);
-void tareaInfrarrojo(void *parameter);
-void tareaUltrasonido(void *parameter);
+
+//------------------- Funciones -------------------
+void recto(int velizq, int veldcha){
+  digitalWrite(PIN_Motor_AIN_1, veldcha);
+  analogWrite(PIN_Motor_PWMA, VEL);
+  
+  digitalWrite(PIN_Motor_BIN_1, velizq);
+  analogWrite(PIN_Motor_PWMB, VEL);
+}
+
+void dcha(int velizq, int veldcha){
+  digitalWrite(PIN_Motor_AIN_1, LOW);
+  analogWrite(PIN_Motor_PWMA, 0);
+  
+  digitalWrite(PIN_Motor_BIN_1, HIGH);
+  analogWrite(PIN_Motor_PWMB, velizq);
+}
+
+void izq(int velizq, int veldcha){
+  digitalWrite(PIN_Motor_AIN_1, HIGH);
+  analogWrite(PIN_Motor_PWMA, veldcha);
+  
+  digitalWrite(PIN_Motor_BIN_1, LOW);
+  analogWrite(PIN_Motor_PWMB, 0);
+}
+
+
+void calcularPD(int valorSensorIzquierdo, int valorSensorCentral, int valorSensorDerecho, int umbral, int &velocidadDerecha, int &velocidadIzquierda) {
+  // Calcula el error basado en los sensores y el umbral
+  // Nota: Este es un ejemplo simple, puedes ajustar la lógica según tus necesidades
+  int error = valorSensorCentral - umbral;
+
+  // Cálculo del PD
+  int delta_error = error - error_anterior;
+  float salidaPD = Kp * error + Kd * delta_error;
+
+  // Actualiza el error anterior
+  error_anterior = error;
+
+  // Calcula las velocidades
+  velocidadDerecha = VEL + salidaPD;
+  velocidadIzquierda = VEL - salidaPD;
+
+  // Limita las velocidades para que estén dentro de los rangos permitidos
+  velocidadDerecha = constrain(velocidadDerecha, 0, 255);
+  velocidadIzquierda = constrain(velocidadIzquierda, 0, 255);
+}
+
+
+void seguidor(){
+  // Lectura de valores de los sensores infrarrojos
+  leftSensorValue = analogRead(PIN_ITR20001_LEFT);
+  middleSensorValue = analogRead(PIN_ITR20001_MIDDLE);
+  rightSensorValue = analogRead(PIN_ITR20001_RIGHT);
+
+  // Calcula las velocidades de los motores
+  int velocidadDerecha, velocidadIzquierda;
+  calcularPD(leftSensorValue, middleSensorValue, rightSensorValue, umbral, velocidadDerecha, velocidadIzquierda);
+
+  if (distance >= 8){
+    if (middleSensorValue > umbral) {
+      recto(velocidadIzquierda, velocidadDerecha);
+    }
+    else if (rightSensorValue > umbral) {
+      dcha(velocidadIzquierda, velocidadDerecha);
+    }
+    else if (leftSensorValue > umbral) {
+      izq(velocidadIzquierda, velocidadDerecha);
+    }
+  } else {
+    digitalWrite(PIN_Motor_AIN_1, LOW);
+    analogWrite(PIN_Motor_PWMA, 0);
+    
+    digitalWrite(PIN_Motor_BIN_1, LOW);
+    analogWrite(PIN_Motor_PWMB, 0);
+  }
+}
+
+
+void ultrasonido(){
+  long t; //timepo que demora en llegar el eco
+  long d; //distancia en centimetros
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(TRIG_PIN, LOW);
+  
+  t = pulseIn(ECHO_PIN, HIGH); //obtenemos el ancho del pulso
+  distance = t/59;             //escalamos el tiempo a una distancia en cm
+  
+  Serial.print("Distancia: ");
+  Serial.print(distance);      //Enviamos serialmente el valor de la distancia
+  Serial.print("cm");
+  Serial.println();
+}
+
+// Task for line following
+void TaskLineFollower(void *pvParameters) {
+  (void) pvParameters;
+
+  for (;;) {
+    seguidor();
+  }
+}
+
+// Task for line following
+void TaskLedBlink(void *pvParameters) {
+  (void) pvParameters;
+
+  for (;;) {
+      // Leer el valor del sensor infrarrojo
+      int middleSensorValue = analogRead(PIN_ITR20001_MIDDLE);
+
+      if (middleSensorValue > umbral) {
+        // Dentro de la línea (verde)
+        FastLED.showColor(CRGB::Green);
+      } else {
+        // Fuera de la línea (rojo)
+        FastLED.showColor(CRGB::Red);
+      }
+
+      FastLED.show();
+  }
+}
+
+// Task for ultrasonic sensor reading
+void TaskUltrasonicSensor(void *pvParameters) {
+  (void) pvParameters;
+
+  for (;;) {
+    ultrasonido();
+    vTaskDelay(100 / portTICK_PERIOD_MS); // Adjust the delay as necessary
+  }
+}
+
+
 
 
 //------------------- Setup -------------------
@@ -86,214 +224,22 @@ void setup() {
 
   digitalWrite(PIN_Motor_STBY, HIGH);
 
+  // LED
+  FastLED.addLeds<NEOPIXEL, PIN_RBGLED>(leds, NUM_LEDS);
+  FastLED.setBrightness(20);
+
   // Configuración pines de infrarrojo
   pinMode(PIN_ITR20001_LEFT, INPUT);
   pinMode(PIN_ITR20001_MIDDLE, INPUT);
   pinMode(PIN_ITR20001_RIGHT, INPUT);
 
-  // LED
-  FastLED.addLeds<NEOPIXEL, PIN_RBGLED>(leds, NUM_LEDS);
-  FastLED.setBrightness(20);
-
-  // Crear los semáforos
-  semaforoInfrarrojo = xSemaphoreCreateBinary();
-  semaforoUltrasonido = xSemaphoreCreateBinary();
-
-  // Crear tareas
-  xTaskCreate(tareaLED, "LEDTask", 128, NULL, 1, NULL);
-  xTaskCreate(tareaInfrarrojo, "InfrarrojoTask", 128, NULL, 2, NULL);
-  xTaskCreate(tareaUltrasonido, "UltrasonidoTask", 128, NULL, 3, NULL);
-
-  vTaskStartScheduler(); // Iniciar el planificador de tareas de FreeRTOS
-
-
-}
-
-//------------------- Funciones -------------------
-void tareaInfrarrojo(void *parameter) {
-  while (1) {
-    // Leer los valores de los sensores infrarrojos
-    leftSensorValue = analogRead(PIN_ITR20001_LEFT);
-    middleSensorValue = analogRead(PIN_ITR20001_MIDDLE);
-    rightSensorValue = analogRead(PIN_ITR20001_RIGHT);
-
-    // Señalar a la tarea del LED
-    xSemaphoreGive(semaforoInfrarrojo);
-
-    vTaskDelay(50 / portTICK_PERIOD_MS); // Ajusta el tiempo según sea necesario
-    Serial.println("Infrarrojo");
-  }
-}
-
-
-void tareaLED(void *parameter) {
-  while (1) {
-    if (xSemaphoreTake(semaforoInfrarrojo, portMAX_DELAY) == pdTRUE) {
-      // Leer el valor del sensor infrarrojo
-      int middleSensorValue = analogRead(PIN_ITR20001_MIDDLE);
-
-      if (middleSensorValue > umbral) {
-        // Dentro de la línea (verde)
-        FastLED.showColor(CRGB::Green);
-      } else {
-        // Fuera de la línea (rojo)
-        FastLED.showColor(CRGB::Red);
-      }
-
-      FastLED.show();
-    }
-
-    vTaskDelay(50 / portTICK_PERIOD_MS); // Ajusta el tiempo de espera según sea necesario
-    Serial.printl("LED");
-  }
-}
-
-
-void recto(){
-  digitalWrite(PIN_Motor_AIN_1, HIGH);
-  analogWrite(PIN_Motor_PWMA, VEL);
+  // Create tasks for line following and ultrasonic sensing
+  xTaskCreate(TaskLineFollower, "LineFollower", 128, NULL, 1, NULL);
+  xTaskCreate(TaskUltrasonicSensor, "Ultrasonic", 128, NULL, 1, NULL);
+  xTaskCreate(TaskLedBlink, "LED", 128, NULL, 1, NULL);
   
-  digitalWrite(PIN_Motor_BIN_1, HIGH);
-  analogWrite(PIN_Motor_PWMB, VEL);
 }
-
-void dcha(){
-  digitalWrite(PIN_Motor_AIN_1, LOW);
-  analogWrite(PIN_Motor_PWMA, VEL/1.5);
-  
-  digitalWrite(PIN_Motor_BIN_1, HIGH);
-  analogWrite(PIN_Motor_PWMB, VEL/1.5);
-}
-
-void izq(){
-  digitalWrite(PIN_Motor_AIN_1, HIGH);
-  analogWrite(PIN_Motor_PWMA, VEL/1.5);
-  
-  digitalWrite(PIN_Motor_BIN_1, LOW);
-  analogWrite(PIN_Motor_PWMB, VEL/1.5);
-}
-
-
-void tareaSeguidor(void *parameter) {
-  while (1) {
-    // Tu lógica de seguimiento de línea aquí
-    // Lectura de valores de los sensores infrarrojos
-    int leftSensorValue = analogRead(PIN_ITR20001_LEFT);
-    int middleSensorValue = analogRead(PIN_ITR20001_MIDDLE);
-    int rightSensorValue = analogRead(PIN_ITR20001_RIGHT);
-
-    if (middleSensorValue > umbral) {
-      recto();
-    } else if (rightSensorValue > umbral) {
-      dcha();
-    } else if (leftSensorValue > umbral) {
-      izq();
-    }
-
-    vTaskDelay(100 / portTICK_PERIOD_MS); // Ajusta el tiempo de espera según sea necesario
-    Serial.println("seguidor");
-  }
-}
-
-
-
-
-/*
-uint32_t Color(uint8_t r, uint8_t g, uint8_t b)
-{
-  return (((uint32_t)r << 16) | ((uint32_t)g << 8) | b);
-}
-
-void myColor()
-{
-  int r=255,g=0,b=0;
-    for(int i=0;i<255;i++)
-    {
-      FastLED.showColor(Color(r, g, b));
-      r-=1;
-      g+=1;
-      delay(10);
-    }
-    r=0,g=0,b=255;
-    
-    for(int i=0;i<255;i++)
-    {
-      FastLED.showColor(Color(r, g, b));
-      r+=1;
-      b-=1;
-      delay(10);
-    }
-    r=0,g=255,b=0;
-
-    for(int i=0;i<255;i++)
-    {
-      FastLED.showColor(Color(r, g, b));
-      g-=1;
-      b+=1;
-      delay(10);
-    }
-    r=0,g=0,b=0;
-}
-*/
-
-
-
-void seguidor(){
-  // Lectura de valores de los sensores infrarrojos
-  leftSensorValue = analogRead(PIN_ITR20001_LEFT);
-  middleSensorValue = analogRead(PIN_ITR20001_MIDDLE);
-  rightSensorValue = analogRead(PIN_ITR20001_RIGHT);
-
-  if (middleSensorValue > umbral) {
-    recto();
-  }
-  else if (rightSensorValue > umbral) {
-    dcha();
-  }
-  else if (leftSensorValue > umbral) {
-    izq();
-  }
-}
-
-/*
-void ultrasonido(){
-  long t;
-  long d;
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10); // enviar un pulso de 10us
-  digitalWrite(TRIG_PIN, LOW);
-  t = pulseIn(ECHO_PIN, HIGH);
-  d = t/59;
-  Serial.println(d);
-  delay(100);
-}
-*/
-
-void tareaUltrasonido(void *parameter) {
-  while (1) {
-    // Realizar la lógica del sensor de ultrasonido
-    long t;
-    long d;
-    digitalWrite(TRIG_PIN, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG_PIN, LOW);
-    t = pulseIn(ECHO_PIN, HIGH);
-    d = t / 59;
-    Serial.println(d);
-    delay(100);
-
-    vTaskDelay(200 / portTICK_PERIOD_MS); // Ajusta el tiempo según sea necesario
-  }
-}
-
-
 
 void loop() {
-  
-  // ultrasonido();
-
-  //seguidor();
-  
-  //myColor();
-
+  // Empty loop - tasks are running in their own loop
 }
